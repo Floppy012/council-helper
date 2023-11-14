212055e53768353dc338bc16d08bac9e2dd4e8f4
Florian Schmidt <florian@f012.dev>
2023-11-14 09:11:04 +0100

    fix: set page title

--- resources/views/components/layouts/default.blade.php
+++ resources/views/components/layouts/default.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>Council Helper</title>
         @vite(['resources/css/tailwind.css', 'resources/css/app.sass', 'resources/js/app.ts'])
     </head>
     <body class="relative bg-dark-600 text-gray-200 z-0">
