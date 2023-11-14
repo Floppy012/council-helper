186238aec87758ec2693b61b10bf05e33ff194da
Florian Schmidt <florian@f012.dev>
2023-11-14 15:05:01 +0100

    feat: QoL improvements

--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -1,7 +1,12 @@
 <div class="w-1/2 m-auto flex items-center h-screen">
-    <div wire:transition class="w-full">
-        <div class="mb-10">
-            <h3 class="text-2xl text-center">Submit your Droptimizer</h3>
+    <div
+            class="absolute z-0 w-full h-full top-0 left-0 blur-md bg-cover bg-center"
+            style="background-image: url({{asset("images/blizzard/raids/amirdrassil/background.jpg")}})"
+    ></div>
+    <div class="relative z-10 w-full">
+        <div class="mb-8">
+            <h3 class="text-2xl text-center font-bold">Submit your Droptimizer</h3>
+            <div class="mt-1 text-base text-gray-400 text-center">Patchwerk &middot; 1 Boss &middot; 6 Minutes &middot; SimC Nightly &middot; Optimal Raid Buffs</div>
         </div>
 
         <form wire:submit="submit">
@@ -13,15 +18,20 @@
                     wire:model="reportUrl"
                     wire:keydown.enter.debounce.1s="submit"
                 />
-                <x-form.button>
-                    <x-spinner wire:loading />
-                    <span wire:loading>Submitting</span>
-                    <span wire:loading.remove>Submit</span>
+                <x-form.button class="bg-green-600 justify-center">
+                    <span class="inline-flex items-center" wire:loading>Submitting</span>
+                    <span class="inline-flex items-center" wire:loading.remove><i class="fas fa-paper-plane mr-3"></i>Submit</span>
                 </x-form.button>
             </x-form.group>
             <x-form.hint for="reportUrl" />
         </form>
     </div>
+
+    @teleport('body')
+    <div class="absolute top-0 right-0 p-8 flex justify-center">
+        <a class="button text-sm" href="{{ route('encounter-select', ['raid' => 'amirdrassil']) }}">Show current raid</a>
+    </div>
+    @endteleport
 
 
 </div>
--- resources/views/livewire/report.blade.php
+++ resources/views/livewire/report.blade.php
@@ -4,7 +4,13 @@
             <livewire:report-loading :report="$report" />
         </div>
     @else
-        <h3 class="text-2xl text-center">Your report has been submitted 🎉</h3>
+        <div>
+            <h3 class="text-3xl text-center">Your report has been submitted 🎉</h3>
+            <div class="flex justify-around mt-4">
+                <a class="button w-[45%] text-center text-sm" href="{{ route('landing') }}">Submit another report</a>
+                <a class="button w-[45%] text-center text-sm" href="{{ route('encounter-select', ['raid' => 'amirdrassil']) }}">Show current raid</a>
+            </div>
+        </div>
     @endif
 </div>
 
